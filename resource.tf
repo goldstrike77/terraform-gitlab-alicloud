0613d42b6530816d98f4e1916147e7b363902b89
suzhetao <suzhetao@gmail.com>
2024-12-09 10:24:05 +0800

    deploy

--- resource.tf
+++ resource.tf
@@ -3,9 +3,43 @@ module "alicloud_resource_manager_resource_group" {
   alicloud_resources = var.alicloud_resources
 }
 
-module "alicloud_db_instance" {
-  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//db-instance?ref=v0.1"
-  tags               = var.tags
+module "vpc" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//vpc?ref=v0.1"
+  tags       = var.tags
   alicloud_resources = var.alicloud_resources
-  depends_on         = [module.alicloud_resource_manager_resource_group]
+  depends_on = [module.resource_manager_resource_group]
+}
+
+module "vpc_ipv4_cidr_block" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//vpc-ipv4-cidr-block?ref=v0.1"
+  alicloud_resources = var.alicloud_resources
+  depends_on = [module.vpc]
+}
+
+module "vpc_peer_connection" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//vpc-peer-connection?ref=v0.1"
+  tags       = var.tags
+  alicloud_resources = var.alicloud_resources
+  depends_on = [module.vpc]
+}
+
+module "vswitch" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//vswitch?ref=v0.1"
+  tags       = var.tags
+  alicloud_resources = var.alicloud_resources
+  depends_on = [module.vpc_peer_connection]
+}
+
+module "route_table" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//route-table?ref=v0.1"
+  tags       = var.tags
+  alicloud_resources = var.alicloud_resources
+  depends_on = [module.vswitch]
+}
+
+module "security_group" {
+  source     = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//security-group?ref=v0.1"
+  tags       = var.tags
+  alicloud_resources = var.alicloud_resources
+  depends_on = [module.vpc]
 }
